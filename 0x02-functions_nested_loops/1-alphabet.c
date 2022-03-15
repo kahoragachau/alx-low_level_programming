91caf7d107851c003a467e7fb30a9bb36fd5b12d
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-15 22:52:18 +0300

    - print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Function that prints _putchar
+ * Return: 0 value returned if program executes successfully
+*/
+
+int main(void)
+{
+    int i;
+
+    for (i=97; i <= 122; i++)
+    {
+        _putchar(i);
+    }
+	_putchar('\n');
+}
