e812d4697def014b3f5f78e872dae8fe1b438aa3
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-18 17:11:34 +0300

    print numbers in specified range

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
 /**
- * print_numbers - prints the numbers, from 0 to 9,
+ * print_numbers - prints numbers from 0 to 9
  * followed by a new line
  */
 
 void print_numbers(void)
 {
-	int i;
+	int m = 0;
 
-	for (i = 0; i < 10; i++)
+	while (m < 10)
 	{
-		_putchar(i + '0');
+		_putchar(m + '0');
+		m++;
 	}
-
 	_putchar('\n');
 }
