945f2710a6e86987bf007bbf80e98c07d79ae096
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-18 16:52:15 +0300

    print numbers between 0 to 10

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* print_numbers - prints numbers between 0-9
+*/
+
+void print_numbers(void)
+{
+	int j;
+
+	for (j = 0; j < 10; j++)
+	{
+		_putchar(i + "0");
+	}
+
+	_putchar("\n")
+}
