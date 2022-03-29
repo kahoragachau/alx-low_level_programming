19c9fdd0596cfe8839340c4d6340cbff244c8bdb
kahoragachau <joelkahoragachau6@gmail.com>
2022-03-29 15:31:16 +0300

    Return a string folllowed by a new line

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,11 +1,16 @@
 #include "main.h"
 
 /**
- * _puts - function to print a string followed by a new line
- * @str: Pointer Parameter
-*/
-
+ * _puts - prints a string to stdout
+ * @str: pointer to the string to print
+ */
 void _puts(char *str)
 {
-	_putchar ("\n", str);
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
 }
