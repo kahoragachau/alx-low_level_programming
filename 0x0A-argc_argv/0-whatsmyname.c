521b880701bccc1f4d386ba79af446edff149b80
kahoragachau <joelkahoragachau6@gmail.com>
2022-04-05 18:35:10 +0300

    -Print new name if program is renamed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Is the Main Function
+ * @argc: Is the number of command line arguments
+ * @argv: is the vector/array containing  the program command line
+ * Return: return 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv);
+	return (0);
+}
