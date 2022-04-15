d49c0eeed518e919ac659d5a5afd535060241b1c
kahoragachau <joelkahoragachau6@gmail.com>
2022-04-15 19:14:24 +0300

    -program returns the name of file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+
+/**
+ * main - the main function to return the file name followed by newline
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
