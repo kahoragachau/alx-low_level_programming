27ba5ce035f3cf151221daf2e9f5512de734a849
kahoragachau <joelkahoragachau6@gmail.com>
2022-05-03 20:33:06 +0300

    - Fix Betty Format error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
-/***
- * read_textfile - eads a text file and prints it to the std output
- * @filename: target file to be read
- * @lettters: the number of letters to be read
+/**
+ * read_textfile - reads a text file and prints it to the standard output
+ * @filename: name of the file to be read
+ * @letters: number of letters to read and print
  *
- * Return: the number of letter to be printed, prints 0 if it fails
+ * Return: the number of letters printed, or 0 if it failed
  */
-
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
